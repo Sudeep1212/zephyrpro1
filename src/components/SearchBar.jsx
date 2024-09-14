d854f2accffcc4a40afb6452f3eb87055b09bbbc
HarishP23 <harish1707pandey@gmail.com>
2024-09-14 19:23:41 +0530

    EventPage and Navbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,19 +1,20 @@
 import React, { useState } from 'react';
 
 const SearchBar = ({ onSearch }) => {
-  const [searchTerm, setSearchTerm] = useState(""); // Define local state
+  const [searchTerm, setSearchTerm] = useState("");
 
   const handleChange = (e) => {
-    setSearchTerm(e.target.value); // Update local state
-    onSearch(e.target.value); // Call the function passed via props
+    setSearchTerm(e.target.value);
+    onSearch(e.target.value);
   };
 
   return (
-    <input 
-      type="text" 
-      placeholder="Search events..." 
+    <input
+      type="text"
+      placeholder="Search events..."
       value={searchTerm}
       onChange={handleChange}
+      className="border border-gray-300 text-black py-2 px-4 rounded-lg w-64 shadow focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-blue-500"
     />
   );
 };
